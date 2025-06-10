49473b30cb5901e9c3ea98184ecbfa0e1e882bb0
Guilherme Kolakowski <guilhermekl99@gmail.com>
2025-06-10 16:55:04 -0300

    Esqueleto do projeto

---
+++ Projeto_final_mic/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 6/10/2025 4:36:23 PM
+ *  Author: Guilherme
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
