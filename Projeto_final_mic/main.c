3d523bd9de02b92b3e06f202267d8965202c072e
Raian Lobato <raian.lv19@aluno.ifsc.edu.br>
2025-06-29 21:39:44 -0300

    Criação do menu de opções (Atualizado, foi enviado apenas o debug)

--- Projeto_final_mic/main.c
+++ Projeto_final_mic/main.c
@@ -1,77 +1,61 @@
-/*
- * main.c
- *
- * Created: 6/10/2025 1:29:09 PM
- *  Author: Guilherme
- */ 
 #define F_CPU 16000000
 #include <xc.h>
 #include <util/delay.h>
 #include <string.h>
+#include <stdio.h>
 
 #define tam_string 100
 char mensagem_enviada[tam_string]= "";
 char mensagem_recebida[tam_string]= "";
 
 uint8_t menu_exibido = 0;
-uint8_t select_sentido = 1;
-
+uint8_t estado_menu = 0;
+uint8_t duty = 0;
 // ----------- UART -----------
 void enviar_mensagem(const char *mensagem_enviada) {
 	for (int i = 0; i < tam_string; i++) {
-		while ((UCSR0A & (1 << UDRE0)) == 0) {
-			// Espera at? o buffer estar pronto para envio
-		}
-
-		if (mensagem_enviada[i] == 0) {
-			break; // Fim da string
-		}
+		while ((UCSR0A & (1 << UDRE0)) == 0) {}
+		if (mensagem_enviada[i] == 0) break;
 		UDR0 = mensagem_enviada[i];
 	}
 	_delay_ms(10);
 	UDR0 = '\n';
 }
-void receber_mensagem(){
-	if((UCSR0A & (1<<RXC0)) != 0){
-		//Verifica se tem algo para receber
-		char byte_recebido = UDR0;
-		if(byte_recebido == '1'){
-			select_sentido = 1;
-		}else if(byte_recebido == '0'){
-			select_sentido = 0;
-		}
+
+char receber_mensagem(){
+	if ((UCSR0A & (1<<RXC0)) != 0) {
+		return UDR0;
+		} else {
+		return '\0';
 	}
 }
 
 // ----------- PWM CONFIG -----------
 
 void ativar_pwmA() {
-	TCCR0A |=  (1 << COM0A1); // Ativa OC0A (PD6)
-	TCCR0A &= ~(1 << COM0B1); // Desativa OC0B (PD5)
-	PORTD &= ~(1 << PD5);     // Garante PD5 em 0V
+	TCCR0A |=  (1 << COM0A1);
+	TCCR0A &= ~(1 << COM0B1);
+	PORTD &= ~(1 << PD5);
 }
 
 void ativar_pwmB() {
-	TCCR0A |=  (1 << COM0B1); // Ativa OC0B (PD5)
-	TCCR0A &= ~(1 << COM0A1); // Desativa OC0A (PD6)
-	PORTD &= ~(1 << PD6);     // Garante PD6 em 0V
+	TCCR0A |=  (1 << COM0B1);
+	TCCR0A &= ~(1 << COM0A1);
+	PORTD &= ~(1 << PD6);
 }
 
 void set_dutyA(uint8_t porcentagem) {
-	if (porcentagem > 100) {
-		porcentagem = 100;
-	}
+	if (porcentagem > 100) porcentagem = 100;
 	OCR0A = (porcentagem * 255) / 100;
 }
 
 void set_dutyB(uint8_t porcentagem) {
-	if (porcentagem > 100) {
-		porcentagem = 100;
-	}
+	if (porcentagem > 100) porcentagem = 100;
 	OCR0B = (porcentagem * 255) / 100;
 }
 
 void exibir_menu() {
+	enviar_mensagem("\n");
 	enviar_mensagem("----- MENU -----");
 	enviar_mensagem("1. Selecionar sentido (0 = A | 1 = B)");
 	enviar_mensagem("2. Setar PWM (0 a 100)");
@@ -80,36 +64,110 @@ void exibir_menu() {
 }
 
 int main(void) {
-
-	// PD5 e PD6 como saida
 	DDRD |= (1 << DDD6) | (1 << DDD5);
 
-	// PWM Timer0 - Fast PWM
 	TCCR0A |= (1 << WGM01) | (1 << WGM00);
 	TCCR0B |= (1 << CS01) | (1 << CS00); // Prescaler 64 (976 Hz)
 
+	UBRR0 = 103;
+	UCSR0A = 0;
+	UCSR0B = (1<<RXEN0) | (1<<TXEN0);
+	UCSR0C = 0b00000110;
 
-	//Comunicacao serial
-	UBRR0 = 103; //CONFIGURANDO BAUD RATE = 9600 pois u2x0 ? 0
-	UCSR0A = 0; // U2X0 = 0 (AFETA O BAUDRATE) se U2X0 = 1 muda a formula e pega metade da frequencia
-	UCSR0B = (1<<RXEN0) | (1<<TXEN0); // 0b00011000
-	UCSR0C = 0b00000110; //00 para setar assincrono. UPM1 E UMP0 desligados. USBS mexe no stopbit, final da informa??o longo ou curto. Setando UCsZ1 e 0 para 8bits. UCPOLmodo assincrono ? 0
-	
+	char recebido;
+
 	while (1) {
-		if(menu_exibido == 0){ //exibir o menu 1 vez
+		if (menu_exibido == 0) {
 			exibir_menu();
 			menu_exibido = 1;
 		}
-		if (select_sentido == 0) {
-			ativar_pwmA();
-			set_dutyA(50);
-		} else if(select_sentido == 1) {
-			ativar_pwmB();
-			set_dutyB(50);
-		}else{
-			//	Enviar uma mensagem de erro
-			//enviar_mensagem("DIRECAO INVALIDA");
+
+		recebido = receber_mensagem();
+
+		if (recebido != '\0') {
+			// Ignora Enter
+			if (recebido == '\n' || recebido == '\r') {
+				continue;
+			}
+			if (recebido == '0') {
+				estado_menu = 0;
+				menu_exibido = 0;
+				continue;
+			}
+
+			switch (estado_menu) {
+				case 0:
+				if (recebido == '1') {
+					estado_menu = 1;
+					enviar_mensagem("\n");
+					enviar_mensagem("SLECIONE O SENTIDO DE GIRO DESEJADO:");
+					enviar_mensagem("1 - DIREITA");
+					enviar_mensagem("2 - ESQUERDA");
+					enviar_mensagem("Digite 0 para voltar");
+					} else if (recebido == '2') {
+					estado_menu = 2;
+					enviar_mensagem("\n");
+					enviar_mensagem("SLECIONE O PWM DESEJADO ex(50%):");
+					enviar_mensagem("Digite 0 para voltar");
+					} else if (recebido == '3') {
+					estado_menu = 3;
+					enviar_mensagem("\n");
+					enviar_mensagem("PWM atual:");
+					enviar_mensagem("RPM atual:");
+					enviar_mensagem("Digite 0 para voltar");
+					} else {
+					enviar_mensagem("\n");
+					enviar_mensagem("Opcao invalida.");
+				}
+				break;
+
+				case 1:
+				if (recebido == '1') {
+					ativar_pwmB();
+					enviar_mensagem("Direcao: DIREITA ativada.");
+					} else if (recebido == '2') {
+					ativar_pwmA();
+					enviar_mensagem("Direcao: ESQUERDA ativada.");
+					} else {
+					enviar_mensagem("Opcao invalida. Digite 1 ou 2 ou 0 para voltar.");
+				}
+				break;
+
+				case 2:
+				enviar_mensagem("\nDigite o valor do PWM (0 a 100):");
+				_delay_ms(500);  // pequena pausa opcional
+
+				char c1 = receber_mensagem();
+				char c2 = receber_mensagem();
+				char c3 = receber_mensagem();
+
+				// Monta número (ex: '7''5' ? 75)
+				int valor = 0;
+				if (c1 >= '0' && c1 <= '9') {
+					valor = (c1 - '0');
+
+					if (c2 >= '0' && c2 <= '9') {
+						valor = valor * 10 + (c2 - '0');
+
+						if (c3 >= '0' && c3 <= '9') {
+							valor = valor * 10 + (c3 - '0');
+						}
+					}
+				}
+
+				if (valor >= 0 && valor <= 100) {
+					duty = valor;
+					set_dutyA(duty);
+					set_dutyB(duty);
+					} else {
+					enviar_mensagem("Valor invalido. Digite entre 0 e 100.");
+				}
+				break;
+
+				case 3:
+				break;
+			}
+
 		}
-		receber_mensagem();
 	}
 }
