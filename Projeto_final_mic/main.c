fdff2b54dc8f9095761ddd124baf45d91e314854
Guilherme Kolakowski <guilhermekl99@gmail.com>
2025-07-03 09:06:49 -0300

    Foi removida a parte de criação de menu para facilitar no desenvolvimento do projeto, e também foi adicionada um função que faz a leitura dos caracteres via monitor serial e armazena num buffer. Neste caso, para fim de testar o funcionamento o buffer é enviado de volta ao usuário via monitor serial

--- Projeto_final_mic/main.c
+++ Projeto_final_mic/main.c
@@ -1,103 +1,103 @@
-/*
- * main.c
- *
- * Created: 6/10/2025 1:29:09 PM
- *  Author: Guilherme
- */ 
 #define F_CPU 16000000
 #include <xc.h>
 #include <util/delay.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define tam_string 100
-//exemplo3
-char mensagem_enviada[tam_string]= "";
-char mensagem_recebida[tam_string]= "";
+char mensagem_enviada[tam_string] = "";
 
-uint8_t select_sentido = 1;
+uint8_t duty = 0;
+uint8_t canal_pwm_ativo = 0; // 1 = PWM A, 2 = PWM B
 
 // ----------- UART -----------
+
 void enviar_mensagem(const char *mensagem_enviada) {
 	for (int i = 0; i < tam_string; i++) {
-		while ((UCSR0A & (1 << UDRE0)) == 0) {
-			// Espera at� o buffer estar pronto para envio
-		}
-
-		if (mensagem_enviada[i] == 0) {
-			break; // Fim da string
-		}
+		while ((UCSR0A & (1 << UDRE0)) == 0) {}
+		if (mensagem_enviada[i] == 0) break;
 		UDR0 = mensagem_enviada[i];
 	}
 	_delay_ms(10);
 	UDR0 = '\n';
 }
-void receber_mensagem(){
-	if((UCSR0A & (1<<RXC0)) != 0){
-		//Verifica se tem algo para receber
-		char byte_recebido = UDR0;
-		if(byte_recebido == '1'){
-			select_sentido = 1;
-		}else if(byte_recebido == '0'){
-			select_sentido = 0;
+
+void receber_mensagem() {
+	char buffer[tam_string] = "";
+	uint8_t i = 0;
+	char caractere;
+
+	while (1) {
+		// Espera até receber um dado
+		while (!(UCSR0A & (1 << RXC0)));
+
+		caractere = UDR0;
+
+		// Se for Enter ('\n' ou '\r'), encerra a string
+		if (caractere == '\n' || caractere == '\r') {
+			// Ignora enter duplicado (ex: \r\n)
+			if (i == 0) continue; // ignora enter sem mensagem
+			buffer[i] = '\0';
+			break;
+		}
+
+		// Armazena no buffer se não ultrapassar o tamanho
+		if (i < tam_string - 1) {
+			buffer[i++] = caractere;
 		}
 	}
+
+	enviar_mensagem("Mensagem recebida:");
+	enviar_mensagem(buffer);
 }
 
 // ----------- PWM CONFIG -----------
 
 void ativar_pwmA() {
-	TCCR0A |=  (1 << COM0A1); // Ativa OC0A (PD6)
-	TCCR0A &= ~(1 << COM0B1); // Desativa OC0B (PD5)
-	PORTD &= ~(1 << PD5);     // Garante PD5 em 0V
+	TCCR0A |= (1 << COM0A1);
+	TCCR0A &= ~(1 << COM0B1);
+	PORTD &= ~(1 << PD5); // Garante 0V no outro canal
+	canal_pwm_ativo = 1;
 }
 
 void ativar_pwmB() {
-	TCCR0A |=  (1 << COM0B1); // Ativa OC0B (PD5)
-	TCCR0A &= ~(1 << COM0A1); // Desativa OC0A (PD6)
-	PORTD &= ~(1 << PD6);     // Garante PD6 em 0V
+	TCCR0A |= (1 << COM0B1);
+	TCCR0A &= ~(1 << COM0A1);
+	PORTD &= ~(1 << PD6); // Garante 0V no outro canal
+	canal_pwm_ativo = 2;
 }
 
 void set_dutyA(uint8_t porcentagem) {
-	if (porcentagem > 100) {
-		porcentagem = 100;
-	}
+	if (porcentagem > 100) porcentagem = 100;
 	OCR0A = (porcentagem * 255) / 100;
 }
 
 void set_dutyB(uint8_t porcentagem) {
-	if (porcentagem > 100) {
-		porcentagem = 100;
-	}
+	if (porcentagem > 100) porcentagem = 100;
 	OCR0B = (porcentagem * 255) / 100;
 }
 
 
 int main(void) {
-
-	// PD5 e PD6 como sa�da
 	DDRD |= (1 << DDD6) | (1 << DDD5);
 
-	// PWM Timer0 - Fast PWM
+	// Configura PWM modo Fast PWM
 	TCCR0A |= (1 << WGM01) | (1 << WGM00);
-	TCCR0B |= (1 << CS01) | (1 << CS00); // Prescaler 64 (976 Hz)
+	TCCR0B |= (1 << CS01) | (1 << CS00); // Prescaler 64 (~976Hz)
+
+	// Configura UART
+	UBRR0 = 103; // 9600 baud
+	UCSR0A = 0;
+	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+	UCSR0C = 0b00000110;
 
 
-	//Comunica��o serial
-	UBRR0 = 103; //CONFIGURANDO BAUD RATE = 9600 pois u2x0 � 0
-	UCSR0A = 0; // U2X0 = 0 (AFETA O BAUDRATE) se U2X0 = 1 muda a formula e pega metade da frequencia
-	UCSR0B = (1<<RXEN0) | (1<<TXEN0); // 0b00011000
-	UCSR0C = 0b00000110; //00 para setar assincrono. UPM1 E UMP0 desligados. USBS mexe no stopbit, final da informa��o longo ou curto. Setando UCsZ1 e 0 para 8bits. UCPOLmodo assincrono � 0
-	
+	// Inicializa PWM com canal A ativo por padrão e duty 0
+	ativar_pwmA();
+	set_dutyA(50);
+
 	while (1) {
-		if (select_sentido == 0) {
-			ativar_pwmA();
-			set_dutyA(50);
-		} else if(select_sentido == 1) {
-			ativar_pwmB();
-			set_dutyB(50);
-		}else{
-			//	Enviar uma mensagem de erro
-			//enviar_mensagem("DIRECAO INVALIDA");
-		}
 		receber_mensagem();
 	}
 }
