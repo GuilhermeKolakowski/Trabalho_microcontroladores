978d6528f4a2da1332d857eccf35c37ad335f5b1
Guilherme Kolakowski <guilhermekl99@gmail.com>
2025-07-08 16:14:01 -0300

    Adicionado leitura do encoder no pino PD2

--- Projeto_final_mic/main.c
+++ Projeto_final_mic/main.c
@@ -1,115 +1,136 @@
-/*
- * main.c
- *
- * Created: 6/10/2025 1:29:09 PM
- *  Author: Guilherme
- */ 
 #define F_CPU 16000000
 #include <xc.h>
 #include <util/delay.h>
-#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
 
 #define tam_string 100
-char mensagem_enviada[tam_string]= "";
-char mensagem_recebida[tam_string]= "";
+char mensagem_enviada[tam_string] = "";
 
-uint8_t menu_exibido = 0;
-uint8_t select_sentido = 1;
+volatile uint16_t contagem_pulsos = 0;
+uint8_t duty = 0;
+uint8_t canal_pwm_ativo = 0; // 1 = PWM A, 2 = PWM B
 
 // ----------- UART -----------
+
 void enviar_mensagem(const char *mensagem_enviada) {
 	for (int i = 0; i < tam_string; i++) {
-		while ((UCSR0A & (1 << UDRE0)) == 0) {
-			// Espera at? o buffer estar pronto para envio
-		}
-
-		if (mensagem_enviada[i] == 0) {
-			break; // Fim da string
-		}
+		while ((UCSR0A & (1 << UDRE0)) == 0);
+		if (mensagem_enviada[i] == 0) break;
 		UDR0 = mensagem_enviada[i];
 	}
 	_delay_ms(10);
 	UDR0 = '\n';
 }
-void receber_mensagem(){
-	if((UCSR0A & (1<<RXC0)) != 0){
-		//Verifica se tem algo para receber
-		char byte_recebido = UDR0;
-		if(byte_recebido == '1'){
-			select_sentido = 1;
-		}else if(byte_recebido == '0'){
-			select_sentido = 0;
+
+void receber_mensagem() {
+	char buffer[tam_string] = "";
+	uint8_t i = 0;
+	char caractere;
+
+	while (1) {
+		while (!(UCSR0A & (1 << RXC0)));
+
+		caractere = UDR0;
+
+		if (caractere == '\n' || caractere == '\r') {
+			if (i == 0) continue;
+			buffer[i] = '\0';
+			break;
+		}
+
+		if (i < tam_string - 1) {
+			buffer[i++] = caractere;
 		}
 	}
+
+	enviar_mensagem("Mensagem recebida:");
+	enviar_mensagem(buffer);
 }
 
-// ----------- PWM CONFIG -----------
+// ----------- ENCODER e RPM -----------
+
+ISR(INT0_vect) {
+	contagem_pulsos++;
+}
+
+ISR(TIMER1_COMPA_vect) {
+	char buffer[32];
+	uint16_t rpm = contagem_pulsos * 60;
+	contagem_pulsos = 0;
+
+	sprintf(buffer, "RPM: %u", rpm);
+	enviar_mensagem(buffer);
+}
+
+void configurar_encoder() {
+	DDRD &= ~(1 << PD4);    // PD4 como entrada
+	PORTD |= (1 << PD4);    // Pull-up ativado
+
+	EICRA |= (1 << ISC01);  // Interrupção na borda de descida
+	EICRA &= ~(1 << ISC00);
+	EIMSK |= (1 << INT0);   // Habilita INT0
+}
+
+void configurar_timer1_para_1s() {
+	TCCR1A = 0;
+	TCCR1B = (1 << WGM12) | (1 << CS12) | (1 << CS10); // Modo CTC, Prescaler 1024
+	OCR1A = 15624; // (16MHz / 1024) * 1s - 1
+	TIMSK1 |= (1 << OCIE1A); // Interrupção por comparação
+}
+
+// ----------- PWM -----------
 
 void ativar_pwmA() {
-	TCCR0A |=  (1 << COM0A1); // Ativa OC0A (PD6)
-	TCCR0A &= ~(1 << COM0B1); // Desativa OC0B (PD5)
-	PORTD &= ~(1 << PD5);     // Garante PD5 em 0V
+	TCCR0A |= (1 << COM0A1);
+	TCCR0A &= ~(1 << COM0B1);
+	PORTD &= ~(1 << PD5); // Garante 0V no outro canal
+	canal_pwm_ativo = 1;
 }
 
 void ativar_pwmB() {
-	TCCR0A |=  (1 << COM0B1); // Ativa OC0B (PD5)
-	TCCR0A &= ~(1 << COM0A1); // Desativa OC0A (PD6)
-	PORTD &= ~(1 << PD6);     // Garante PD6 em 0V
+	TCCR0A |= (1 << COM0B1);
+	TCCR0A &= ~(1 << COM0A1);
+	PORTD &= ~(1 << PD6); // Garante 0V no outro canal
+	canal_pwm_ativo = 2;
 }
 
 void set_dutyA(uint8_t porcentagem) {
-	if (porcentagem > 100) {
-		porcentagem = 100;
-	}
+	if (porcentagem > 100) porcentagem = 100;
 	OCR0A = (porcentagem * 255) / 100;
 }
 
 void set_dutyB(uint8_t porcentagem) {
-	if (porcentagem > 100) {
-		porcentagem = 100;
-	}
+	if (porcentagem > 100) porcentagem = 100;
 	OCR0B = (porcentagem * 255) / 100;
 }
 
-void exibir_menu() {
-	enviar_mensagem("----- MENU -----");
-	enviar_mensagem("1. Selecionar sentido (0 = A | 1 = B)");
-	enviar_mensagem("2. Setar PWM (0 a 100)");
-	enviar_mensagem("3. Exibir RPM e PWM atual");
-	enviar_mensagem("Digite uma opcao:");
-}
-
 int main(void) {
+	// PWM pinos como saída
+	DDRD |= (1 << PD6) | (1 << PD5);
 
-	// PD5 e PD6 como saida
-	DDRD |= (1 << DDD6) | (1 << DDD5);
+	// UART
+	UBRR0 = 103; // 9600 baud
+	UCSR0A = 0;
+	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+	UCSR0C = 0b00000110;
 
-	// PWM Timer0 - Fast PWM
+	// PWM modo Fast PWM, Prescaler 64
 	TCCR0A |= (1 << WGM01) | (1 << WGM00);
-	TCCR0B |= (1 << CS01) | (1 << CS00); // Prescaler 64 (976 Hz)
+	TCCR0B |= (1 << CS01) | (1 << CS00);
 
+	// Configurações
+	configurar_encoder();
+	configurar_timer1_para_1s();
 
-	//Comunicacao serial
-	UBRR0 = 103; //CONFIGURANDO BAUD RATE = 9600 pois u2x0 ? 0
-	UCSR0A = 0; // U2X0 = 0 (AFETA O BAUDRATE) se U2X0 = 1 muda a formula e pega metade da frequencia
-	UCSR0B = (1<<RXEN0) | (1<<TXEN0); // 0b00011000
-	UCSR0C = 0b00000110; //00 para setar assincrono. UPM1 E UMP0 desligados. USBS mexe no stopbit, final da informa??o longo ou curto. Setando UCsZ1 e 0 para 8bits. UCPOLmodo assincrono ? 0
-	
+	// Inicializa PWM
+	ativar_pwmA();
+	set_dutyA(50);
+
+	sei();
+
 	while (1) {
-		if(menu_exibido == 0){ //exibir o menu 1 vez
-			exibir_menu();
-			menu_exibido = 1;
-		}
-		if (select_sentido == 0) {
-			ativar_pwmA();
-			set_dutyA(50);
-		} else if(select_sentido == 1) {
-			ativar_pwmB();
-			set_dutyB(50);
-		}else{
-			//	Enviar uma mensagem de erro
-			//enviar_mensagem("DIRECAO INVALIDA");
-		}
-		receber_mensagem();
+		receber_mensagem(); // Aguarda e exibe mensagens do usuário
 	}
 }
